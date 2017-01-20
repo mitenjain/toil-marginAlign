1cdef2f60084f7bf51b454edbfec6bc786478a72
ArtRand <arand@soe.ucsc.edu>
2017-01-20 09:58:03 -0800

    integrated new vcf writer

--- src/toil_nanopore/marginCallerToil.py
+++ src/toil_nanopore/marginCallerToil.py
@@ -5,8 +5,8 @@ from toil_lib import require
 from margin.toil.realign import shardSamJobFunction
 from margin.toil.variantCaller import\
     calculateAlignedPairsJobFunction,\
-    combineVcfShardsJobFunction,\
     marginalizePosteriorProbsJobFunction
+from margin.toil.variantCall import makeVcfFromVariantCalls
 from margin.toil.alignment import shardAlignmentByRegion
 from margin.toil.stats import marginStatsJobFunction
 from margin.toil.hmm import downloadHmm
@@ -14,10 +14,9 @@ from margin.toil.hmm import downloadHmm
 
 def marginCallerJobFunction(job, config, input_samfile_fid, output_label):
     require(input_samfile_fid is not None, "[marginCallerJobFunction]input_samfile_fid is NONE")
-    # split up the large alignment
+    # split up the large alignment by region
     smaller_alns        = shardAlignmentByRegion(job, config, input_samfile_fid)
-    #smaller_alns        = splitLargeAlignment(job, config, input_samfile_fid)
-    vcf_shards          = []
+    all_variant_calls   = []
     hidden_markov_model = downloadHmm(job, config)
     # this loop runs through the smaller alignments and sets a child job to get the aligned pairs for 
     # each one. then it marginalizes over the columns in the alignment and adds a promise of a dict containing
@@ -25,16 +24,16 @@ def marginCallerJobFunction(job, config, input_samfile_fid, output_label):
     job.fileStore.logToMaster("[marginCallerJobFunction]Have {} smaller alignments to variant "
                               "call".format(len(smaller_alns)))
     for aln in smaller_alns:
-        disk      = input_samfile_fid.size + config["reference_FileStoreID"].size
-        memory    = (6 * input_samfile_fid.size)
-        vcf_shard = job.addChildJobFn(shardSamJobFunction,
-                                      config, aln, hidden_markov_model,
-                                      calculateAlignedPairsJobFunction,
-                                      marginalizePosteriorProbsJobFunction,
-                                      disk=disk, memory=memory).rv()
-        vcf_shards.append(vcf_shard)
+        disk          = input_samfile_fid.size + config["reference_FileStoreID"].size
+        memory        = (6 * input_samfile_fid.size)
+        variant_calls = job.addChildJobFn(shardSamJobFunction,
+                                          config, aln, hidden_markov_model,
+                                          calculateAlignedPairsJobFunction,
+                                          marginalizePosteriorProbsJobFunction,
+                                          disk=disk, memory=memory).rv()
+        all_variant_calls.append(variant_calls)
 
-    job.addFollowOnJobFn(combineVcfShardsJobFunction, config, vcf_shards, output_label)
+    job.addFollowOnJobFn(makeVcfFromVariantCalls, config, all_variant_calls, output_label)
 
     if config["stats"]:
         job.addFollowOnJobFn(marginStatsJobFunction, config, input_samfile_fid, output_label,
