bb52bfc462b814e81a4dc2eee6ae6cdc8f23b52b
ArtRand <arand@soe.ucsc.edu>
2017-02-04 11:39:05 -0800

    fixed stats up, ||'ed it

--- src/toil_nanopore/marginCallerToil.py
+++ src/toil_nanopore/marginCallerToil.py
@@ -7,7 +7,7 @@ from margin.toil.variantCaller import\
     calculateAlignedPairsJobFunction,\
     marginalizePosteriorProbsJobFunction
 from margin.toil.variantCall import makeVcfFromVariantCallsJobFunction2
-from margin.toil.stats import marginStatsJobFunction
+from margin.toil.stats import collectAlignmentStatsJobFunction
 from margin.toil.hmm import downloadHmm
 
 
@@ -17,7 +17,7 @@ def marginCallerJobFunction(job, config, input_samfile_fid, smaller_alns, output
     hidden_markov_model = downloadHmm(job, config)
     # this loop runs through the smaller alignments and sets a child job to get the aligned pairs for 
     # each one. then it marginalizes over the columns in the alignment and adds a promise of a dict containing
-    # the posteriors to the list `expctations`
+    # the posteriors to the list `all_variant_calls`
     count = 0
     for aln in smaller_alns:
         disk          = input_samfile_fid.size + config["reference_FileStoreID"].size
@@ -33,6 +33,5 @@ def marginCallerJobFunction(job, config, input_samfile_fid, smaller_alns, output
     job.addFollowOnJobFn(makeVcfFromVariantCallsJobFunction2, config, all_variant_calls, output_label)
 
     if config["stats"]:
-        raise NotImplementedError
-        #job.addFollowOnJobFn(marginStatsJobFunction, config, input_samfile_fid, output_label,
-        #                     memory=input_samfile_fid.size)
+        job.addFollowOnJobFn(collectAlignmentStatsJobFunction, config, input_samfile_fid, output_label,
+                             memory=input_samfile_fid.size)
