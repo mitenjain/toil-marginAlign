5dee5838c0ff8621d197ecb16ad8f49e1b9dc5b4
ArtRand <arand@soe.ucsc.edu>
2017-03-19 18:17:08 -0700

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name="toil-nanopore",
-      version="0.2.30",
+      version="0.2.41",
       description="Toil-script for performing MinION sequence analysis",
       author="Benedict Paten and Art Rand",
       author_email="benedict@soe.ucsc.edu, arand@soe.ucsc.edu",
@@ -11,7 +11,7 @@ setup(name="toil-nanopore",
       package_dir={"": "src"},
       packages=find_packages("src"),
       install_requires=["PyYAML==3.12",
-                        "marginAlign==1.1.7",
+                        "marginAlign==1.1.9",
                         "toil-lib==1.2.0a1.dev139"],
       entry_points={
           "console_scripts": ["toil-nanopore = toil_nanopore.toil_nanopore_pipeline:main"]})
